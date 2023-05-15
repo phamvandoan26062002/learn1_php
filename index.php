222685dc6ad8e8ea8ec8804265fa967dfc7fd936
phamvandoan26062002 <phamvandoan26062002@gmail.com>
2023-05-15 14:14:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,18 @@ function sum ($a, $b){
 }
 echo sum (a:1 , b:2);
 
-function dthcn($a){
+function dthv($a){
     return $a*$a;
 }
-echo dthcn (a:1);
+echo "Dien tich hinh vuong la:".dthv (a:1);
+
+#dùng biến globals
+$a = 1;
+$b = 2;
+
+function dientich(){
+    $GLOBALS['s'] = $GLOBALS['a'] * $GLOBALS['b'] * 1/2;
+}
+dientich();
+echo 'Diện tích hình tam giác:'.$s;
 ?>
