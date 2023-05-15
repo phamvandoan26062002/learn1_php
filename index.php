149eb395eaaaa96f2181bd60343e8a6d8fc362cf
phamvandoan26062002 <phamvandoan26062002@gmail.com>
2023-05-15 15:58:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
-<?php
-/*ECHO'phạm văn đoan';
-
-function sum ($a, $b){
-    return $a + $b;
-}
-echo sum (a:1 , b:2);
-
-function dthv($a){
-    return $a*$a;
-}
-echo "Dien tich hinh vuong la:".dthv (a:1);
-
-#dùng biến globals
-$a = 1;
-$b = 2;
-
-function dientich(){
-    $GLOBALS['s'] = $GLOBALS['a'] * $GLOBALS['b'] * 1/2;
-}
-dientich();
-echo 'Diện tích hình tam giác:'.$s; */
-
-#Xuất ra trình duyệt chuỗi ‘Hello World !’
-ECHO 'Hello World !';
-
-#Xác định số phần tử của chuỗi 'HoangVanSang'
- echo strlen('HoangVanSang');
-
-?>
+<!-- Viết một chương trình PHP để thêm một chuỗi vào đầu hoặc cuối của một chuỗi sử dụng hàm str_pad(). -->
+$str1 = "freetuts.net";
+$str2 = "freetuts";
+echo strcmp($str1, $str2);
