2c96e9389f3e6971acd2c1fb823d143d7152a9fe
phamvandoan26062002 <phamvandoan26062002@gmail.com>
2023-07-17 21:09:01 +0700

    Update update.php

--- hwday8/Products/update.php
+++ hwday8/Products/update.php
@@ -2,13 +2,30 @@
 require_once 'pdo.php';
 require_once 'helper.php';
 
+class CategoryManager {
+    private $database;
+
+    public function __construct($database) {
+        $this->database = $database;
+    }
+
+    public function updateCategory($category) {
+        $this->database->update($category['name'], $category['id']);
+    }
+}
+
 $request = $_POST;
 
-$category = [
-    'name' =>$request['name'],
-    'id'=>$request['id']
-];
+if (isset($request['name']) && isset($request['id'])) {
+    $category = [
+        'name' => $request['name'],
+        'id' => $request['id']
+    ];
 
-update($category);
-redirectHome();
+    $categoryManager = new CategoryManager($database);
+    $categoryManager->updateCategory($category);
+    redirectHome();
+} else {
+    die('Invalid request');
+}
 ?>
