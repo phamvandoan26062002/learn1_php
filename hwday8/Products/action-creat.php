9e0fa375501963febd1f9a7467d2a3f47200b10a
phamvandoan26062002 <phamvandoan26062002@gmail.com>
2023-07-17 21:14:51 +0700

    update

--- hwday8/Products/action-creat.php
+++ hwday8/Products/action-creat.php
@@ -1,11 +1,26 @@
 <?php
-    require_once 'pdo.php';
-    $data = [
-        'prodId' => '',
-        'prodName' => $_POST['name'],
-        'prodPrice' => $_POST['price'],
-        'cateId' => $_POST['cateId']
-    ];
-    createNewProdData($data);
-    header("Location: http://localhost/hoc.php/product/index.php");
+require_once 'pdo.php';
+
+class ProductManager {
+    private $database;
+
+    public function __construct($database) {
+        $this->database = $database;
+    }
+
+    public function createProduct($data) {
+        $this->database->create($data);
+    }
+}
+
+$data = [
+    'prodId' => '',
+    'prodName' => $_POST['name'],
+    'prodPrice' => $_POST['price'],
+    'cateId' => $_POST['cateId']
+];
+
+$productManager = new ProductManager($database);
+$productManager->createProduct($data);
+header("Location: http://localhost/hoc.php/product/index.php");
 ?>
--- hwday8/Products/action-update.php
+++ hwday8/Products/action-update.php
@@ -1,11 +1,26 @@
 <?php 
-    require_once "pdo.php";
-    $data = [
-        'prodName' => $_POST['name'],
-        'prodPrice' => $_POST['price'],
-        'cateId' => $_POST['cateId'],
-        'id' => $_GET['id']
-    ];
-    updateProdData($data);
-    header("Location: http://localhost/learn_php/product/index.php");
+require_once "pdo.php";
+
+class ProductManager {
+    private $database;
+
+    public function __construct($database) {
+        $this->database = $database;
+    }
+
+    public function updateProduct($data) {
+        $this->database->update($data['prodName'], $data['prodPrice'], $data['cateId'], $data['id']);
+    }
+}
+
+$data = [
+    'prodName' => $_POST['name'],
+    'prodPrice' => $_POST['price'],
+    'cateId' => $_POST['cateId'],
+    'id' => $_GET['id']
+];
+
+$productManager = new ProductManager($database);
+$productManager->updateProduct($data);
+header("Location: http://localhost/learn_php/product/index.php");
 ?>
