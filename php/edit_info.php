64f4d7e73ca942f497c835d48ac204ae7d4990ee
phamvandoan26062002 <phamvandoan26062002@gmail.com>
2023-07-30 17:02:23 +0700

    update

--- php/edit_info.php
+++ php/edit_info.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="./images_of_Đoan/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="./profile.css">
+    <link rel="stylesheet" href="./edit_info.css">
 </head>
 <body>
     <?php require "./header.php"; ?>
@@ -17,7 +17,6 @@
         <div class="container">
             <div class="header">
                 <h4>CHỈNH SỬA THÔNG TIN</h4>
-                <a href="profile.php" class="cancelButton">Hủy bỏ</a>
             </div>
             <div class="info">
                 <?php
@@ -45,21 +44,24 @@
 
                     // Form chỉnh sửa thông tin
                     echo '<form action="update_info.php?id=' . $id_khach_hang . '" method="POST">';
-                    echo '<label for="ten_nha_thuoc">Tên nhà thuốc:</label>';
+                    echo '<label for="ten_nha_thuoc"><strong class="label">Tên nhà thuốc</strong></label>';
                     echo '<input type="text" id="ten_nha_thuoc" name="ten_nha_thuoc" value="' . $row["ten_nha_thuoc"] . '"><br>';
-                    echo '<label for="nguoi_lien_he">Người liên hệ:</label>';
+                    echo '<label for="nguoi_lien_he"><strong class="label">Người liên hệ</strong></label>';
                     echo '<input type="text" id="nguoi_lien_he" name="nguoi_lien_he" value="' . $row["nguoi_lien_he"] . '"><br>';
-                    echo '<label for="so_dien_thoai">Số điện thoại:</label>';
+                    echo '<label for="so_dien_thoai"><strong class="label">Số điện thoại</strong></label>';
                     echo '<input type="text" id="so_dien_thoai" name="so_dien_thoai" value="' . $row["so_dien_thoai"] . '"><br>';
-                    echo '<label for="dia_chi">Địa chỉ:</label>';
+                    echo '<label for="dia_chi"><strong class="label">Địa chỉ</strong></label>';
                     echo '<input type="text" id="dia_chi" name="dia_chi" value="' . $row["dia_chi"] . '"><br>';
-                    echo '<label for="so_GPP">Số GPP:</label>';
+                    echo '<label for="so_GPP"><strong class="label">Số GPP</strong></label>';
                     echo '<input type="text" id="so_GPP" name="so_GPP" value="' . $row["so_GPP"] . '"><br>';
-                    echo '<label for="ngay_cap">Ngày cấp:</label>';
+                    echo '<label for="ngay_cap"><strong class="label">Ngày cấp</strong></label>';
                     echo '<input type="text" id="ngay_cap" name="ngay_cap" value="' . $row["ngay_cap"] . '"><br>';
-                    echo '<label for="noi_cap">Nơi cấp:</label>';
+                    echo '<label for="noi_cap"><strong class="label">Nơi cấp</strong></label>';
                     echo '<input type="text" id="noi_cap" name="noi_cap" value="' . $row["noi_cap"] . '"><br>';
-                    echo '<input type="submit" value="Xác nhận chỉnh sửa">';
+                    echo '<div class="button-container">';
+                    echo '<input type="submit" class="btn_cancel" value="Hủy bỏ" formaction="profile.php">';
+                    echo '<input type="submit" class="btn_update" value="Chỉnh sửa">';
+                    echo '</div>';
                     echo '</form>';
                 } else {
                     echo "Không tìm thấy thông tin khách hàng.";
